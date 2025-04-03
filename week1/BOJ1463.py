29776e40ca2793d4d14748eda72b150d1857a474
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-04-03 17:06:21 +0900

    [BOJ] 1463 / 실버3

--- week1/BOJ1463.py
+++ week1/BOJ1463.py
@@ -1,20 +1,29 @@
-import sys
+# 절단기 높이가 낮을수록 잘리는 나무는 많고, 절단기 높이가 높을수록 잘리는 나무는 적음 - 이진탐색 사용하자!
+# H 값을 조정해서 잘린 나무의 총합이 M 이상이 되는지
 
-N = int(input())
+def binary_search(trees, M):
+    start = 0
+    end = max(trees)
+    result = 0
 
-# dp[i]: 숫자 i를 1로 만드는 최소 연산 횟수
-dp = [0] * (N + 1)
+    while start <= end:
+        mid = (start + end) // 2
 
-for i in range(2, N + 1):
-    # 이전 숫자(i-1)에서 1을 빼서 만든 경우
-    dp[i] = dp[i - 1] + 1
-    
-    # 2로 나누어 떨어질 때
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
+        total = 0
+        for tree in trees:
+            if tree > mid:
+                total += tree - mid
 
-    # 3으로 나누어 떨어질 때
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i // 3] + 1)
+        if total >= M:
+            result = mid  # 가능한 높이일 경우 저장
+            start = mid + 1  # 더 높은 것도 가능한지 탐색
+        else:
+            end = mid - 1
 
-print(dp[N])
+    return result
+
+
+N, M = map(int, input().split())    # 나무 개수, 필요한 나무 길이
+trees = list(map(int, input().split())) # 나무 높이
+
+print(binary_search(trees, M))
